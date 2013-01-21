4b1cd3a925e9dd779ff403fd6f020f7411861a03
ivanyu <ivanhoe12@gmail.com>
2013-01-21 20:55:48 +0400

    Code formatting.

--- pa3/part1.R
+++ pa3/part1.R
@@ -1,3 +1,5 @@
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 outcome[, 11] <- as.numeric(outcome[, 11])
-hist(outcome[, 11], xlab="30-day Death Rate", main="Heart Attack 30-day Death Rate")
+hist(outcome[, 11],
+    xlab="30-day Death Rate",
+    main="Heart Attack 30-day Death Rate")
--- pa3/part3.R
+++ pa3/part3.R
@@ -10,7 +10,12 @@ state <- outcome2$State
 by_median=reorder(state, death, median, na.rm=TRUE)
 
 par(las=2, cex=0.7)
-boxplot(death ~ by_median, xaxt="n", ylab="30-day Death Rate", main="Heart Attack 30-day Death Rate by State")
+boxplot(death ~ by_median,
+    xaxt="n",
+    ylab="30-day Death Rate",
+    main="Heart Attack 30-day Death Rate by State")
 tab <- table(by_median)
 s <- names(tab)
-axis(1, 1:length(s), paste0(s, " (", as.numeric(tab), ")"), las=2, cex.axis=0.8)
+axis(1,
+    1:length(s),
+    paste0(s, " (", as.numeric(tab), ")"), las=2, cex.axis=0.8)
